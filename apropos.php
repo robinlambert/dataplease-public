63f7f280c58d7a47276d9dd76bb348d7d3ce374b
Robin Lambert <robin.lambert@live.fr>
2014-01-15 10:42:37 +0100

    First commit

---
+++ apropos.php
@@ -0,0 +1,25 @@
+<?php include "templates/header.php"; ?>
+
+      <!-- Begin page content -->
+      <div class="container">
+        <div class="page-header">
+          <h1>À propos</h1>
+        </div>
+        <h2 class="lead">Qui est derrière DataPlease ?</h2>
+        <p>Ce projet est réalisé par 6 étudiants de l'école <a href="http://hetic.net">HETIC</a> dans le cadre d'un projet à visée pédagogique.</p>
+        <p><a target="_blank" href="https://twitter.com/Gilles_Bertaux">Gilles Bertaux</a>, <a target="_blank" href="https://twitter.com/RemiFayolle">Rémi Fayolle</a>, <a target="_blank" href="https://twitter.com/tom_forlini">Tom Forlini</a>, <a target="_blank" href="https://twitter.com/VincentGarreau">Vincent Garreau</a>, <a target="_blank" href="https://twitter.com/Robin_Lambert">Robin Lambert</a>, <a target="_blank" href="https://twitter.com/FloMonteil">Florentin Monteil</a></p>
+        <h2 class="lead">Pourquoi cette API ?</h2>
+        <p>La vocation première de cette API est d'encourager les initiatives open data. L'INSEE représente une source de données libres incroyable mais difficilement exploitable. Dataplease permet de faciliter l'accès à ces données et d'encourager le développement d'applications innovantes.</p>
+         <h2 class="lead">Nous contacter</h2>
+         <p>Par mail à l'adresse: <a mailto="">dataplease@gmail.com</a></p>
+
+      </div>
+    </div>
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="js/jquery.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+  </body>
+</html>
---
+++ index.php
@@ -0,0 +1,20 @@
+<?php include "templates/header.php"; ?>
+
+      <!-- Begin page content -->
+      <div class="container">
+        <div class="page-header">
+          <h1>Les données de l'INSEE en quelques millisecondes</h1>
+        </div>
+        <p class="lead">Réutilisez l’ensemble des données de recensement de l’INSEE avec notre API full Javascript.</p>
+        <p><a class="btn btn-primary" href="http://datapleasedoc.herokuapp.com/dataplease">Commencer à utiliser l'API</a></p>
+      </div>
+    </div>
+
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="js/jquery.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+  </body>
+</html>
